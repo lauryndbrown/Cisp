062e3d4e759c64dbf3c0a2e4ca06044fb705d24a
lauryndbrown <lauryndbrown@gmail.com>
2017-04-15 22:17:32 -0400

    Created hello world COBOL program.

---
+++ cisp.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author: Lauryn Brown
+      * Date: 2017
+      * Purpose: COBOL Common Lisp Interpreter
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CISP.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM CISP.
