3379542eee2624507dcf6b5834136a20618ae759
lauryndbrown <lauryndbrown@gmail.com>
2017-04-28 16:00:37 -0400

    Completed bat file rewrite.

--- bin/cisp.bat
+++ bin/cisp.bat
@@ -6,7 +6,9 @@ for /f  "delims=" %%d in ('dir /b %test_dir%') do (
 ECHO --------------------------
 ECHO Test Directory: %%d
     for /f  "delims=" %%f in ('dir /b "%test_dir%%%d"') do (
-        ECHO File: %test_dir%%%d\%%f & echo.
+        ECHO File:%test_dir%%%d\%%f
+        TYPE %test_dir%%%d\%%f 
+        CALL cisp.exe %test_dir%%%d\%%f
     )
 )
 PAUSE
