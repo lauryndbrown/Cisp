2142e201f7e247b2678c9249630fdc01b970f8f1
lauryndbrown <lauryndbrown@gmail.com>
2017-04-28 15:54:15 -0400

    Rewrote cisp.bat file.

--- bin/cisp.bat
+++ bin/cisp.bat
@@ -1,10 +1,13 @@
 cls
 @ECHO OFF
-for /f "delims=" %%i in ('dir /b  "..\test\*.lisp"') do (
-    ECHO cisp.exe ..\test\%%i
-    TYPE ..\test\%%i
-    CALL cisp.exe ..\test\%%i
-    ECHO --------------------------
+SET test_dir=..\test\
+ECHO %test_dir%
+for /f  "delims=" %%d in ('dir /b %test_dir%') do (
+ECHO --------------------------
+ECHO Test Directory: %%d
+    for /f  "delims=" %%f in ('dir /b "%test_dir%%%d"') do (
+        ECHO File: %test_dir%%%d\%%f & echo.
+    )
 )
 PAUSE
 EXIT
